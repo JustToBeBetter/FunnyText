9e67dfbc487f675ad19358ef8eea1682d8b68a87
JustToBeBetter <li@li.local>
2016-03-31 11:48:22 +0800

    Initial Commit

---
+++ FunnyText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunnyText
+//
+//  Created by 李金柱 on 16/3/31.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunnyText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunnyText
+//
+//  Created by 李金柱 on 16/3/31.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
